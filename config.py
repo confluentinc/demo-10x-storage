320b6be0eeca930c1d4b0ff517d2e99ca642d170
chuck-confluent <chuck@confluent.io>
2022-07-08 13:08:49 -0700

    work on stuff

--- config.py
+++ config.py
@@ -7,5 +7,9 @@ load_dotenv()
 
 
 KAFKA_CONFIG = {
-    "":""
+    "": os.environ["CCLOUD_BOOTSTRAP_ENDPOINT"],
+    "": os.environ["CCLOUD_CLUSTER_API_KEY"],
+    "": os.environ["CCLOUD_CLUSTER_API_SECRET"],
+    "security.protocol": "SASL_SSL",
+    "sasl.mechanisms": "PLAIN"
 }
